a1a8539478df8ee407e288acced434f08bd6117c
Maher <02James.MAher@gulllakecs.org>
2024-03-12 13:51:47 -0400

    Added for loop called count

--- count1-10Aipythonyoutube.py
+++ count1-10Aipythonyoutube.py
@@ -1,5 +1,7 @@
 # Programmer : James Maher
 #Date 3.12.2024
 #program count 1-10
-#resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjmuAoIA
+#resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjmuAoIA\
 
+for count in range(1, 11):
+    print(count)
