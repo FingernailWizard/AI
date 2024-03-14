1e6a210e85c3def433429a714b59cc9b1829cf44
Maher <02James.MAher@gulllakecs.org>
2024-03-14 14:02:25 -0400

    PrintingNamesIndex Stable - pre chatgpt

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -2,3 +2,10 @@
 #DAte 3.14.2024
 #program PringtingNamesIndex
 # Resourse https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+names = ["Corey","Chris","Travis","Dave"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
